00c723a9e027f29d6b4f95e9c05425834883732b
yusuf <kebbeyusuf@gmail.com>
2023-12-21 22:57:29 +0300

    landing page close open the menu func. implementation

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const menuIcon = document.querySelector(".menu-icon");
 const container = document.querySelector(".container");
 
+const landing = document.querySelector('.landing')
+
+
+landing.addEventListener('click', () => {
+
+  container.classList.remove('change')
+
+})
+
 menuIcon.addEventListener("click", () => {
   container.classList.toggle("change");
 });
